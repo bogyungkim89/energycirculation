a78c1d20cf46252d29a3e9c77e9861f7616e7021
Bogyung Kim <bogyungkim89@gmail.com>
2025-09-01 15:24:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,11 @@ html_code = """
                 </marker>
             </defs>
             <path d="M 420 188.33 L 420 280 C 420 310, 390 330, 336 330 C 282 330, 252 310, 252 280 L 252 100" stroke="blue" stroke-width="40" fill="none" marker-end="url(#u-arrowhead)" />
+            <text x="252" y="75" font-family="Arial" font-size="20" fill="white" text-anchor="middle" font-weight="bold">대기반사</text>
+            <text x="252" y="100" font-family="Arial" font-size="20" fill="white" text-anchor="middle" font-weight="bold">25</text>
             <path d="M 432 253.8 L 432 433.8 C 432 463.8, 381.8 493.8, 280.2 493.8 C 178.6 493.8, 128.4 463.8, 128.4 433.8 L 128.4 75.3" stroke="blue" stroke-width="13.33" fill="none" marker-end="url(#u-arrowhead)" />
+            <text x="280" y="80" font-family="Arial" font-size="20" fill="blue" text-anchor="middle" font-weight="bold">지표반사</text>
+            <text x="280" y="105" font-family="Arial" font-size="20" fill="blue" text-anchor="middle" font-weight="bold">5</text>
         </svg>
     </div>
 </div>
