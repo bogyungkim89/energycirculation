9d9928baaf1e7306a3c32334f48805d5fd22e87b
Bogyung Kim <bogyungkim89@gmail.com>
2025-09-01 14:12:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ st.markdown(
 
 html_code = """
 <div style="display: flex; justify-content: center; align-items: center; width: 100%;">
-    <div style="width: 1200px; height: 600px; border: 2px solid black; display: flex; flex-direction: column;">
-        <div style="flex: 1; background-color: #302C44; border-bottom: 2px solid black; display: flex; justify-content: flex-start; align-items: center; font-size: 21px; padding-left: 20px; color: white;">
+    <div style="width: 1200px; height: 600px; display: flex; flex-direction: column;">
+        <div style="flex: 1; background-color: #302C44; display: flex; justify-content: flex-start; align-items: center; font-size: 21px; padding-left: 20px; color: white;">
             상단 영역
         </div>
-        <div style="flex: 1.4; background-color: #CEEBF0; border-top: 2px solid black; border-bottom: 2px solid black; display: flex; justify-content: flex-start; align-items: center; font-size: 21px; padding-left: 20px;">
+        <div style="flex: 1.4; background-color: #CEEBF0; display: flex; justify-content: flex-start; align-items: center; font-size: 21px; padding-left: 20px;">
             중간 영역
         </div>
         <div style="height: 100px; background-color: #ABC53C; display: flex; justify-content: flex-start; align-items: center; font-size: 21px; padding-left: 20px;">
