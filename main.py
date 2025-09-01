9af72a4a7e20ca34c8a89b105c18a4dcf87d3ec1
Bogyung Kim <bogyungkim89@gmail.com>
2025-09-01 14:01:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ st.title("지구 열수지 알아보기")
 
 html_code = """
 <div style="display: flex; justify-content: center; align-items: center; width: 100%;">
-    <div style="width: 600px; height: 300px; border: 2px solid black; display: flex;">
-        <div style="flex: 1; background-color: #f0f8ff; border-right: 2px solid black; display: flex; justify-content: center; align-items: center; font-size: 20px;">
-            영역 1
+    <div style="width: 600px; height: 300px; border: 2px solid black; display: flex; flex-direction: column;">
+        <div style="flex: 1; background-color: #f0f8ff; border-bottom: 2px solid black; display: flex; justify-content: center; align-items: center; font-size: 20px;">
+            상단 영역
         </div>
-        <div style="flex: 1; background-color: #f5fffa; border-right: 2px solid black; display: flex; justify-content: center; align-items: center; font-size: 20px;">
-            영역 2
+        <div style="flex: 1; background-color: #f5fffa; border-bottom: 2px solid black; display: flex; justify-content: center; align-items: center; font-size: 20px;">
+            중간 영역
         </div>
         <div style="flex: 1; background-color: #fff0f5; display: flex; justify-content: center; align-items: center; font-size: 20px;">
-            영역 3
+            하단 영역
         </div>
     </div>
 </div>
